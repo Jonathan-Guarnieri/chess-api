a64bf65aa2e98642222bf08ce3b3addab3991642
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2025-03-24 22:24:03 -0300

    create an action to test GameChannel

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,9 +1,16 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    # stream_from "some_channel"
+    # game_id = params[:id]
+    # stream_from "game_#{game_id}"
+    stream_from "game_1"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
+  end
+
+  def test
+    # ActionCable.server.broadcast("game_1", {message: 'test'})
+    transmit({ message: 'test ok' })
   end
 end
