1178b25ac12ddb267a5f75074ca94b9fc3d361f1
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2025-04-21 21:03:56 -0300

    update game channel to support multiple games using game ids

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,16 +1,24 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    # game_id = params[:id]
-    # stream_from "game_#{game_id}"
-    stream_from "game_1"
+    game_id = params[:id]
+    stream_from "game_#{game_id}"
   end
 
   def unsubscribed
     # Any cleanup needed when channel is unsubscribed
   end
 
-  def test
-    # ActionCable.server.broadcast("game_1", {message: 'test'})
-    transmit({ message: 'test ok' })
+  def move(data)
+    # validator = MoveValidator.new(from_square: data['from'], to_square: data['to'], board_state:)
+    broadcast_to "game_#{params[:id]}", {
+      action: 'move_validator_result',
+      valid: true 
+    }
   end
+
+  # private
+
+  # def board_state
+  #   RedisClientWrapper.instance.get()
+  # end
 end
