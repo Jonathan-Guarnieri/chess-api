09e6cf28fc21f1d415bab63999b689dfe65569e3
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2025-05-05 21:09:59 -0300

    adapt Game Channel to receives game_id as opts

--- app/channels/game_channel.rb
+++ app/channels/game_channel.rb
@@ -1,6 +1,10 @@
 class GameChannel < ApplicationCable::Channel
   def subscribed
-    game_id = params[:id]
+    game_id = params.dig(:opts, :gameId)
+    unless game_id.is_a?(Integer)
+      raise "unable to find a valid game_id from params: #{params}"
+    end
+
     stream_from "game_#{game_id}"
   end
 
