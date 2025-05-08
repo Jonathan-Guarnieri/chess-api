27bc9246bf3a7ec93d155a766ad75149b162c6d3
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2025-05-07 21:44:48 -0300

    improve matchmaker reliability

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -6,6 +6,13 @@ module ApplicationCable
       self.current_user = find_verified_user
     end
 
+    def disconnect
+      # clean Matchmaker if user disconnects unexpectedly
+      MatchmakerQueue.remove(current_user.id)
+    rescue => e
+      Rails.logger.error("Failed to clean queue on disconnect: #{e.message}")
+    end
+
     private
 
     def find_verified_user
--- app/channels/matchmaker_channel.rb
+++ app/channels/matchmaker_channel.rb
@@ -1,19 +1,25 @@
 class MatchmakerChannel < ApplicationCable::Channel
   def subscribed
-    put_current_user_on_queue
-    stream_from matchmaker_channel
+    if lock_queue
+      begin
+        put_current_user_on_queue unless current_user_already_on_queue
+        stream_from matchmaker_channel
 
-    if queue > 1
-      p "MatchmakerChannel is trying to create a new match!"
-      game = CreateGame.new(players_ids: first_two_players_on_queue).call
-      p "A game was created: #{game}"
+        if queue_size >= 2
+          game = create_game
 
-      broadcast_to matchmaker_channel, {
-        action: 'match_found',
-        game_id: game.id,
-        white_player: game.white_player,
-        black_player: game.black_player
-      }
+          broadcast_to matchmaker_channel, {
+            action: 'match_found',
+            game_id: game.id,
+            white_player: game.white_player,
+            black_player: game.black_player
+          }
+        end
+      ensure
+        remove_lock
+      end
+    else
+      connection.reject_subscription
     end
   end
 
@@ -23,10 +29,6 @@ class MatchmakerChannel < ApplicationCable::Channel
 
   private
 
-  def queue_name
-    "matchmaker"
-  end
-
   def matchmaker_channel
     "matchmaker_channel"
   end
@@ -35,21 +37,49 @@ class MatchmakerChannel < ApplicationCable::Channel
     RedisClientWrapper.instance
   end
 
-  def queue
-    redis.llen(queue_name)
+  def queue_name
+    "matchmaker"
   end
 
-  def remove_current_user_from_queue
-    redis.lrem(queue_name, current_user.id, 0)
-    p "The User with ID #{current_user.id} was just removed from queue"
+  def lock_key
+    "matchmaker_lock"
+  end
+
+  def lock_token
+    @lock_token ||= SecureRandom.uuid
+  end
+
+  def lock_queue
+    redis.set(lock_key, lock_token, nx: true, ex: 3)
+  end
+
+  def remove_lock
+    redis.del(lock_key)
+  end
+
+  def queue_size
+    redis.llen(queue_name)
   end
 
   def put_current_user_on_queue
     redis.rpush(queue_name, current_user.id)
-    p "The User with ID #{current_user.id} just joined into queue"
+    Rails.logger.info "The User with ID #{current_user.id} just joined into queue"
+  end
+
+  def remove_current_user_from_queue
+    redis.lrem(queue_name, current_user.id, 0)
+    Rails.logger.info "The User with ID #{current_user.id} was just removed from queue"
+  end
+
+  def current_user_already_on_queue
+    redis.lpos(queue_name, current_user.id)
   end
 
   def first_two_players_on_queue
     redis.lpop(queue_name, 2)
+  end
+
+  def create_game
+    CreateGame.new(players_ids: first_two_players_on_queue).call
   end
 end
