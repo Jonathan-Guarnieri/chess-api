9d263cc63701f735328ddc8d56028506eeee4840
Jonathan Guarnieri <djou.guarnieri@hotmail.com>
2025-07-16 19:10:32 -0300

    improvements on fen handling inside Game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,5 +1,5 @@
 class Game < ApplicationRecord
-  INITIAL_FEN ||= "rn1qkbnr/pppb1ppp/8/3pp3/8/5NP1/PPPPPPBP/RNBQK2R w KQkq - 0 1".freeze
+  INITIAL_FEN ||= Chess::Game.new.board.to_fen.freeze
 
   belongs_to :white_player, class_name: "User"
   belongs_to :black_player, class_name: "User"
@@ -17,7 +17,7 @@ class Game < ApplicationRecord
   before_validation :set_initial_fen, on: :create
 
   validates :fen, presence: true, format: {
-    with: /\A\s*(?:[rnbqkpRNBQKP1-8]{1,8}\/){7}[rnbqkpRNBQKP1-8]{1,8}\s[wb]\s(?:-|[KQkq]{1,4})\s(?:-|[a-h][36])\s\d+\s\d+\s*\z/i,
+    with: /\A((?:[PRNBQKprnbqk1-8]{1,8}\/){7}[RNBQKPrnbqkp1-8]{1,8})\s(w|b)\s(K?Q?k?q?|-)\s([a-h][1-8]|-)\s(\d+)\s(\d+)\z/,
     message: "must be a valid FEN string"
   }
 
